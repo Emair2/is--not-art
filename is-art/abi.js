f41e65cb018049ffa309563ab27335e619cb7864
Emair2 <yzhu28@saic.edu>
2022-09-20 14:57:26 -0500

    Update abi.js

--- is-art/abi.js
+++ is-art/abi.js
@@ -1,5 +1,39 @@
 // change this each time you deploy a new contract
-var contractAddress = 
+var contractAddress = "0xD2974D3eCaB08345EDCefFb9B365Fdfd509C7aab";
 
 // paste contract ABI here
-var contractABI = 
+var contractABI = [
+	{
+		"inputs": [],
+		"name": "changeStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getStatus",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isArt",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
